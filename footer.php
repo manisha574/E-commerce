af6d6363f7f6696532b22a9e80114b7f491036ee
manisha <110734103+manisha574@users.noreply.github.com>
2024-06-29 17:22:34 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <div class="container">
-        <p>&copy; <?php echo date("Y"); ?> eCommerce Store</p>
-    </div>
-</footer>
-
-<script src="js/script.js"></script>
-</body>
-</html>
